d27fad93c0460384cbd59f947d3156e8bf76eff6
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-11-03 21:12:28 +0530

    added loop in fetchdescription.py to make the process continous until exit event happens

--- fetchdescription.py
+++ fetchdescription.py
@@ -1,28 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-#getting input from user (name of the website)
-url_tag = input("Enter the name of the website that you want to search")
+while True :
+    #getting input from user (name of the website)
+    url_tag = input("Enter the name of the website that you want to search")
 
-#urltag that we get is in the form of string
-url_string = "http://www." + url_tag + ".com"
+    if(url_tag == "EXIT" or url_tag == "exit"):
+        break
 
-#passing the url_string into the get method of request
-url_page = requests.get(url_string)
+    #urltag that we get is in the form of string
+    url_string = "http://www." + url_tag + ".com"
 
-#parsing html document of the site using html parser
-soup = BeautifulSoup(url_page.content, "html.parser")
+    #passing the url_string into the get method of request
+    url_page = requests.get(url_string)
 
-#storing title from the parsed html page
-title = soup.find('title').text.strip()
+    #parsing html document of the site using html parser
+    soup = BeautifulSoup(url_page.content, "html.parser")
 
-#storing description from the parsed html page
-webpage_desc = soup.find('meta', attrs = {'name' : 'description'})
+    #storing title from the parsed html page
+    title = soup.find('title').text.strip()
 
-#getting the attribute content from the meta tag
-desc = webpage_desc.attrs['content']
+    #storing description from the parsed html page
+    webpage_desc = soup.find('meta', attrs = {'name' : 'description'})
 
-#printing title, url, description
-print( "\n"+ str(title))
-print(url_string)
-print(desc)
+    #getting the attribute content from the meta tag
+    desc = webpage_desc.attrs['content']
+
+    #printing title, url, description
+    print( "\n"+ str(title))
+    print(url_string)
+    print(desc)
+
